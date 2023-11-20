460d79f51b4007bd0b0fc22931f48144f4db39d6
wegotta2319 <120082584+wegotta2319@users.noreply.github.com>
2023-11-19 18:45:32 -0800

    Update main.cpp

--- assign4/main.cpp
+++ assign4/main.cpp
@@ -7,15 +7,23 @@
  */
 //You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
 // Remember to add sufficient and clear comments to your code
+#include <random>
+#include <time.h>
+#include <pthread.h>
+#include <semaphore.h>
 #include <iostream>
 #include "buffer.h"
 #include <unistd.h>
+
 
 using namespace std;
 
 // global buffer object
 Buffer buffer;
-
+pthread_mutex_t mtx;
+sem_t full;
+sem_t nothing;
+pthread_attr_t att;
 // Producer thread function
 // TODO: Add your implementation of the producer thread here
 void *producer(void *param) {
@@ -33,6 +41,8 @@ void *producer(void *param) {
         } else {
             cout << "Producer error condition"  << endl;    // shouldn't come here
         }
+        pthread_mutex_unlock (&mtx);
+        sem_post(&full);
     }
 }
 
@@ -45,35 +55,54 @@ void *consumer(void *param) {
         /* sleep for a random period of time */
         usleep(rand() % 1000000);
         // TODO: Add synchronization code here
+        sem_wait(&full);
+        pthread_mutex_lock (&mtx);
+
         if (buffer.remove_item(&item)) {
             cout << "Consumer " << item << ": Removed item " << item << endl;
             buffer.print_buffer();
         } else {
             cout << "Consumer error condition" << endl;    // shouldn't come here
         }
+        pthread_mutex_unlock(&mtx);
+        sem_post(&nothing);
     }
 }
 
 int main(int argc, char *argv[]) {
     /* TODO: 1. Get command line arguments argv[1],argv[2],argv[3] */
-        if (argc != 4) {
-        cout << "Incorrect variable count, usage is as follows: " << endl;
-        cout << "./prog4 argv[1], argv[2], argv[3]" << endl;
-        exit(1);
-    }
-
     /* TODO: 2. Initialize buffer and synchronization primitives */
-
-    int sleepDuration = atoi(argv[1]);
-    int prodThreadCount = atoi(argv[2]);
-    int consThreadCount = atoi(argv[3]);
-
-    pthread_t = prodThreads;
-    pthread_t = consThreads;
-
     /* TODO: 3. Create producer thread(s).
      * You should pass an unique int ID to each producer thread, starting from 1 to number of threads */
     /* TODO: 4. Create consumer thread(s) */
     /* TODO: 5. Main thread sleep */
     /* TODO: 6. Exit */
+    if(argc != 4){
+        cout << "Usage: ./assign4 <sleeptime> <pthreadc> <cthreadc>" << endl;
+        exit(1);
+    }
+
+    int sleepTime = atoi(argv[1]);
+    int pThreadc = atoi(argv[2]);
+    int cThreadc = atoi(argv[3]);
+
+    pthread_t pThreads;
+    pthread_t cThreads;
+
+    pthread_mutex_init (&mtx, nullptr);
+    sem_init(&nothing,0, buffer_size);
+    sem_init(&full,0,0);
+
+    for(int i = 0; i < pThreadc; i++){
+        int *arg = new int;
+
+        if(arg = nullptr){
+            cout << "Can't allocate memory" << endl;
+            exit(0);
+        }
+
+        *arg = i + 1;
+
+        pthread_create(&pThreads, &att, producer,(void*) arg);
+    }
 }
