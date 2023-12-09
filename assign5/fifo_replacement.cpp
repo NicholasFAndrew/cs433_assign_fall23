ff65ef4d028e9f20d2c869fd8a2fe473ff2e42d5
wegotta2319 <120082584+wegotta2319@users.noreply.github.com>
2023-12-08 16:59:45 -0800

    Update fifo_replacement.cpp

--- assign5/fifo_replacement.cpp
+++ assign5/fifo_replacement.cpp
@@ -12,23 +12,42 @@
 
 // TODO: Add your implementation here
 FIFOReplacement::FIFOReplacement(int num_pages, int num_frames)
-: Replacement(num_pages, num_frames)
+: Replacement(num_pages, num_frames) , fifo_queue()
 {
     // TODO: Add additional implementation code
+    /*for(int i = 0; i < num_frames; i++){
+        fifo_queue.push_back(i);
+    }*/
 }
 
 // TODO: Add your implementations for desctructor, load_page, replace_page here
 FIFOReplacement::~FIFOReplacement() {
     // TODO: Add necessary code here
+    while(!fifo_queue.empty()){
+        fifo_queue.pop_back();
+    }
 }
 
 // Access an invalid page, but free frames are available
 void FIFOReplacement::load_page(int page_num) {
     // TODO: Update your data structure FIFO replacement and pagetable
+    page_fault++;
+    page_table[page_num].valid = true;
+    fifo_queue.push_back(page_num);
+    page_table[page_num].frame_num = frame_count;
+    frame_count++;
 }
 
 // Access an invalid page and no free frames are available
 int FIFOReplacement::replace_page(int page_num) {
     // TODO: Update your data structure FIFO replacement and pagetable
+    page_fault++;
+    int front_index = fifo_queue.front();
+    page_table[front_index].valid = false;
+    fifo_queue.erase(fifo_queue.begin());
+    page_table[page_num].valid = true;
+    fifo_queue.push_back(page_num);
+    replaced_page++;
+    
     return 0;
 }
